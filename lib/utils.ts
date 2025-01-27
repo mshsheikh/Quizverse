4c4b70a96099bb862d47b9ad555a4d5c8b8d04ca
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-01-27 22:36:22 +0500

    Changed: TS background color

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const backgroundColors: { [key: string]: string } = {
   NodeJS: "#ecf5e1",
-  TypeScript: "#d0e0f7",
+  TypeScript: "#d7e4f7",
   NextJS: "#eeeeee",
   TailwindCSS: "#f2fcfd",
 };
